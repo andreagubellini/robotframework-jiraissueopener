02dca7ac87c12ea922639cb99ea00027d3129295
Andrea Gubellini <andreagubellini@gmail.com>
2020-03-18 19:51:38 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="robotframework-jiraissueopener",
+    version="0.0.1",
+    author="Andrea Gubellini",
+    author_email="agubellini@yahoo.com",
+    description="An automatic Jira issue opener for Robotframework",
+    long_description=[],
+    long_description_content_type="text/markdown",
+    url="https://github.com/andreagubellini/robotframework-jiraissueopener",
+    package_dir={'': 'src'},
+    install_requires=['robotframework>=3.0', 'requests>=2.20.0'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ])   
--- src/jiraissueopener.py
+++ src/jiraissueopener.py
@@ -1,10 +1,3 @@
-#steps:
-#gather robotframework informations
-#wrap informations
-#give them to a request
-#post a new  bug
-#retrieve bug id
-#stamp the id
 from robot.libraries.BuiltIn import BuiltIn
 from robot.api.deco import keyword
 import requests
@@ -15,9 +8,32 @@ class Error(RuntimeError):
     ROBOT_CONTINUE_ON_FAILURE = True
 
 class jiraissueopener(object):
+    """
+    = Table of contents =
+
+    - `Usage`
+    - `Importing`
+    - `Keywords`
+
+    = Usage =
+
+    With the following library, we can set specific tests to open a jira issue when *failed*.
+    Each issue may have a specific type, be opened on a specific project and assigned to a specific user.
+    """
     ROBOT_LIBRARY_SCOPE = 'TEST CASE'
 
-    def __init__(self, user, password, project_url, project_id):
+    def __init__(self, user=None, password=None, project_url=None, project_id=None):
+        """
+        *user* = Jira username
+
+        *password* = Jira password
+
+        *project_url* = Jira project url. Example: https://127.0.0.1/rest/api/2/issue/
+
+        *project_id* = the project id on which the issue will be opened. Retrieve this from your project's settings.
+
+        *Import example* = ``Library    jiraissueopener   myuser   mypassword   https://127.0.0.1/rest/api/2/issue/   10800``
+        """
         self.user = user
         self.password = password
         self.project = project_url
@@ -25,6 +41,12 @@ class jiraissueopener(object):
 
     @keyword('Open Jira Issue')
     def open_jira_issue(self, issue_id, assignee):
+        """Opens a jira issue.
+        
+        The issue can be assigned automatically to a specific assignee by passing the username of the assignee. Example: agubellini.
+
+        The issue may be opened as a specific type by passing the issue id to "issue_id" parameter. Example: 10100
+        """
         self.test_message = BuiltIn().get_variable_value("${TEST_MESSAGE}")
         self.test_status = BuiltIn().get_variable_value("${TEST_STATUS}")
         self.test_doc = BuiltIn().get_variable_value("${TEST_DOCUMENTATION}")
