9874bfd171f4c9b5fc5a1d304ad2c82583d74974
Andrea Gubellini <andreagubellini@gmail.com>
2020-03-19 19:46:56 +0100

    updated doc + 0.2.0

--- README.md
+++ README.md
@@ -1,4 +1,64 @@
+# robotframework-jiraissueopener
+
 ![Upload Python Package](https://github.com/andreagubellini/robotframework-jiraissueopener/workflows/Upload%20Python%20Package/badge.svg?branch=master) ![Tests](https://github.com/andreagubellini/robotframework-jiraissueopener/workflows/Tests/badge.svg?branch=master)
 
-# Keyword and library documentation
-Full documentation can be found here: http://jiraissueopener.surge.sh/
+With the following listener a user can automatically create Jira issues when tests fail. Each failed test will have an individual Jira issue for which 
+    assignee, project id and issue type may be modified.
+
+## Issue reported data
+
+The issue will report:
+
+**Summary**: `suite_name: test_name has failed` (example: *No-Regression-Suite: Login test has failed*)
+
+**Description**: 
+
+     Error message: test_message
+     Documentation: test_documentation
+
+**Assignee**: assignee can be set as listener argument
+
+## Log files
+Current execution `log.html` file is attached to all opened issues.
+
+## Installation
+```shell
+pip install robotframework-jiraissueopener
+```
+
+## Dependencies
+- Robotframework
+- Requests
+
+NB: above dependencies are installed automatically when using pip to retrieve `robotframework-jiraissueopener`
+
+## Usage
+Executed `.robot` file must have two variables called `${jira_user}` and `${jira_password}`.
+
+Example:
+```
+***Variables***
+${jira_user}   myuser
+${jira_password}   mypassword
+```
+
+The listener arguments are:
+
+`project_url`: your jira server url. Working example: *https://jira.myserver.com/rest/api/2/issue/*
+
+`project_id`: your project id. Retrievable from project settings. Example: *11805*
+
+`assignee`: assignee's username. Example: *agubellini*
+
+`issue_type`: can be found on your jira settings. ([How-to](https://confluence.atlassian.com/jirakb/finding-the-id-for-issue-types-646186508.html)) Example: *10100* 
+
+## Example
+The following is an example based on the dummy data used in **Usage**
+
+`robot --listener "robotframework-jiraissueopener;https://jira.myserver.com/rest/api/2/issue/;11805;agubellini;10100" mytests.robot`
+
+## Contributing
+Pull requests are welcome. Please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/jiraissueopener.py
+++ src/jiraissueopener.py
@@ -3,35 +3,16 @@ from robot.api.deco import keyword
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+from robot.output import LOGGER
+from robot.output.loggerhelper import Message
 
 class jiraissueopener(object):
     """
-    = Table of contents =
-
-    - `Usage`
-    - `Importing`
-    - `Keywords`
-
-    = Usage =
-
-    With the following library, we can set specific tests to open a jira issue when *failed*.
-    Each issue may have a specific type, be opened on a specific project and assigned to a specific user.
+    With the following listener a user can automatically create Jira issues when tests fail. Each failed test will have an individual Jira issue for which 
+    assignee, project id and issue type may be modified.
     """
-    
     ROBOT_LISTENER_API_VERSION = 3
     def __init__(self, project_url=None, project_id=None, assignee=None, issue_type=None):
-        
-        """
-        *user* = Jira username
-
-        *password* = Jira password
-
-        *project_url* = Jira project url. Example: https://127.0.0.1/rest/api/2/issue/
-
-        *project_id* = the project id on which the issue will be opened. Retrieve this from your project's settings.
-
-        *Import example* = ``Library    jiraissueopener   myuser   mypassword   https://127.0.0.1/rest/api/2/issue/   10800``
-        """
         self.project = project_url
         self.project_id = project_id
         self.jira_issues_list = []
@@ -49,25 +30,26 @@ class jiraissueopener(object):
                     "fields":{
                                 "project":  { "id": self.project_id },
                                 "summary": self.suite_name + ": " + result.name + " has failed",
-                                "description": "Error message:\n" + result.message + "\nDocumentation: \n" + result.doc,
+                                "description": "Error message:" + result.message + "\nDocumentation: " + result.doc,
                                 "assignee": {"name": self.assignee },
                                 "issuetype":    { "id": self.issue_type }
                             }
                 }
             self._send_request(payload)
 
-    def log_file(self, log_file):
-        file = log_file.path
+    def log_file(self, path):
+        file = path
+        print(file)
         self._upload_log_files(file)
-        
+ 
 
     def _upload_log_files(self, file):
         headers = {"X-Atlassian-Token": "nocheck"}
         for i in self.jira_issues_list:
-            attachments = {'file': open(self.file, 'rb')}
+            attachments = {'file': open(file, 'rb')}
             try:
                 upload_files = requests.post(self.project+i+"/attachments", files=attachments, headers=headers, auth=HTTPBasicAuth(self.user, self.password))
-                print(upload_files.content)
+                print(upload_files)
             except Exception as e:
                 print(e)
 
@@ -75,10 +57,9 @@ class jiraissueopener(object):
         headers = {"Content-type": "application/json" }
         try:
             request = requests.post(self.project, data=json.dumps(data), headers=headers, auth=HTTPBasicAuth(self.user, self.password))
-            print(request)
             content = dict(request.json())
             jira_issue = content["key"]
-            print(jira_issue)
+            LOGGER.log_message(Message('Opened issue ' + jira_issue , 'INFO'))
             self.jira_issues_list.append(jira_issue)
         except Exception as e:
             print(e)
