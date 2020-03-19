e7a3b64ec9313a5940048a951ded7b4df17aa2c9
Andrea Gubellini <andreagubellini@gmail.com>
2020-03-19 20:16:58 +0100

    0.0.2

--- setup.py
+++ setup.py
@@ -18,4 +18,4 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ])   
+    ])    
