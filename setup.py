82663c125af5a8e2e7c63ee1af6406d4508e8f2d
Andrea Gubellini <andreagubellini@gmail.com>
2020-03-20 13:15:51 +0100

    packaging

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="robotframework-jiraissueopener",
-    version="0.0.2.4",
+    version="0.0.2.5",
     author="Andrea Gubellini",
     author_email="agubellini@yahoo.com",
     description="An automatic Jira issue opener for Robotframework",
     long_description=[],
     long_description_content_type="text/markdown",
     url="https://github.com/andreagubellini/robotframework-jiraissueopener",
-    packages=['src'],
+    packages=['jiraissueopener'],
     install_requires=['robotframework>=3.0', 'requests>=2.20.0'],
     classifiers=[
         "Programming Language :: Python :: 3",
